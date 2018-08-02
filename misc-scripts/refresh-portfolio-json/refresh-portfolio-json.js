bce3e557b174be1dfd7c3a97aa2833d1acdbc8bf
Ely De La Cruz <elycruz@elycruz.com>
2018-08-02 13:18:14 -0400

    rpj-script - Updated rpj-script.

--- misc-scripts/refresh-portfolio-json/refresh-portfolio-json.js
+++ misc-scripts/refresh-portfolio-json/refresh-portfolio-json.js
@@ -1,6 +1,6 @@
 /**
  * Created by elydelacruz on 7/19/16.
- * @description Generate a JSON file which resembles all 'portfolio' directories and their files.
+ * @description Generates a JSON file which resembles all 'portfolio' directories and their files as JSON.
  * @todo Make script reference asset paths relatively.
  */
 
@@ -8,7 +8,7 @@ const
     path = require('path'),
     mime = require('mime'),
     markdown = require('markdown-it')(),
-    {assign, peek, error, log} = require('fjl'),
+    {assign, error, log, compose, head, tail, split} = require('fjl'),
     {ensureOutputPath, ioReadDirectory, ioStat, ioReadFile, ioWriteFile, ioDoesFilePathExists} = require('../utils/utils'),
     {refreshPortfolioImagesConfig, refreshPortfolioJsonC} = require('../../package'),
     entryDefaults = require('./entry-defaults'),
@@ -16,7 +16,8 @@ const
         markdownInputPath:  'src/assets/portfolio-descriptions-markdown',
         inputPath:          refreshPortfolioImagesConfig.outputPathPrefix,
         outputPath:         'src/assets/json',
-        outputFileName:     'portfolios-data.json'
+        outputFileName:     'portfolios-data.json',
+        forHttpSplitFilePathAt: 'src/'
     },
 
     incomingOptions = {...defaultOptions, ...refreshPortfolioJsonC},
@@ -24,48 +25,80 @@ const
     refreshPortfoliosJson = options => {
         const {outputPath, outputFileName} = options;
         return ensureOutputPath(outputPath)
+
+            // Parse 'portfolios' directory
             .then(() => parseDir(options))
-            .then(parsed => peek('\npeek at parsed:', parsed))
-            .then(JSON.stringify.bind(JSON))
-            .then(json => ioWriteFile(path.join(outputPath, outputFileName), json))
+
+            // Only allow directories at top level of portfolios directory
+            .then(parsed => parsed.filter(entry => entry.fileType === 'dir'))
+
+            // Parse `parsed` directories structure to JSON string
+            .then(parsed => JSON.stringify(parsed, null, 5))
+
+            // .then(parsed => peek('\npeek at parsed:', parsed))
+
+            // Write json to file
+            .then(json => {
+                const outputFilePath = path.join(outputPath, outputFileName);
+                return ioWriteFile(outputFilePath, json).then(() => outputFilePath);
+            })
+
+            // Notify user of success
+            .then(outputFilePath => log(`\nJson file written successfully to: ${outputFilePath}`))
+
+            // Fail if failure
             .catch(error);
     },
 
     parseDir = options => {
-        const {inputPath, outputPath, outputFileName, markdownInputPath} = options;
+        const {inputPath, markdownInputPath, forHttpSplitFilePathAt} = options;
         log(`\nParsing file path: ${inputPath} ...`);
+
+        // Get files list for directory at `inputPath`
         return ioReadDirectory(inputPath).then(files =>
+
+            // Parse files in directory at `inputPath`
             Promise.all(files.map(file => {
+
+                    // Join path for file `file`
                     const filePath = path.join(inputPath, file),
-                        httpPathName = filePath.replace(/\\+/, '/').split('/src/').pop(),
+
+                        // Get file path for http friendly use
+                        httpPathName = compose(head, tail, split(forHttpSplitFilePathAt))(filePath.replace(/\\+/, '/')),
+
+                        // Get file entry to output in our JSON
                         entry = {
                             fileName: file,
-                            filePath: httpPathName,
-                            fileType: ''
+                            filePath: httpPathName
                         };
+
+                    // Parse the file at `filePath`
                     return ioStat(filePath).then(stat => {
+
+                        // If is directory get update `entry` and get it's subsequent files list
                         if (stat.isDirectory()) {
                             entry.fileType = 'dir';
                             entry.description = getDirDescr(file, markdownInputPath);
                             log (`\nFound nested directory, parsing it ...`);
-                            return parseDir({inputPath: filePath, outputPath, outputFileName, markdownInputPath})
+                            return parseDir({...options, inputPath: filePath})
                                 .then(f => { entry.files = f; return entry; });
                         }
-                        parseFile({inputPath: filePath, outputPath, outputFileName, markdownInputPath}, entry);
 
+                        // Else parse incoming file
+                        return parseFile({...options, file, filePath}, entry);
                     });
                 })
             ));
     },
 
-    parseFile = (options, entry) =>
+    parseFile = ({file}, entry) =>
         Promise.resolve(
             /\d+\.(jpg|png|jpeg)/.test(file) ? // Make this regex and option of `incomingConfig`
-                assign(entry, {
+                assign({
                     fileType: 'file',
                     ext: path.extname(file),
                     mimeType: mime.getType(file)
-                }) : entry
+                }, entry) : entry
         ),
 
     getDirDescr = (folderName, markdownInputPath) => {
@@ -78,9 +111,3 @@ const
 ;
 
 refreshPortfoliosJson(incomingOptions);
-
-/* SUDO CODE
-    For each in 'portfolios' directory
-
-
- */
